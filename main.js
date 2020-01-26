96418415177e6f8933cd31c22029135c79a3ca23
beremonth <beremonth@hotmail.com>
2020-01-26 15:29:52 -0600

    Funciones con parametros con valor por defecto en Typescript

--- main.js
+++ main.js
@@ -3,22 +3,32 @@
 Nombre archivo: main.ts
 Proposito: hacer pruebas con las clases del curso sobre TypeScript
 
-Ultima modificación: Funciones con parametros opcionales en Typescript
+Ultima modificación: Funciones con parametros con valor por defecto en Typescript
 Autor: Stephen
 Fecha: 26/01/2020
-Hora: 02:43 pm
+Hora: 03:29 pm
 
 */
-// el signo ?, indica que parametro es opcional
-function nombreCompleto(nombre, apellido) {
+// Indicar un valor de por defecto a un parametro, usamos el signo =, pero se convierte en opcional
+function nombreCompleto(nombre, apellido, capitalizar) {
+    if (capitalizar === void 0) { capitalizar = false; }
+    console.log("capitalizar: " + capitalizar);
     // caso donde el apellido Si se recibe 
-    if (apellido) {
-        return nombre + ' ' + apellido;
+    if (capitalizar) {
+        return capitalizarPalabras(nombre) + ' ' + capitalizarPalabras(apellido);
     }
     // caso donde el apellido No se recibe 
     else {
-        return nombre;
+        return nombre + ' ' + apellido;
     }
 } // end nombreCompleto
-var persona = nombreCompleto("Juan");
-console.log(persona);
+function capitalizarPalabras(palabra) {
+    var palabraTransformada;
+    palabraTransformada = palabra.charAt(0).toUpperCase() + palabra.substr(1).toLowerCase();
+    return palabraTransformada;
+} // end capitalizarPalabras
+// indicamos que el argumento tendrá un valor diferte al que se inidicó en la función
+var personaTransformada = nombreCompleto("juan", "dominguez", true);
+var personaOriginal = nombreCompleto("juan", "dominguez");
+console.log(personaTransformada);
+console.log(personaOriginal);
--- main.ts
+++ main.ts
@@ -2,29 +2,43 @@
 Nombre archivo: main.ts
 Proposito: hacer pruebas con las clases del curso sobre TypeScript
 
-Ultima modificación: Funciones con parametros opcionales en Typescript
+Ultima modificación: Funciones con parametros con valor por defecto en Typescript
 Autor: Stephen 
 Fecha: 26/01/2020
-Hora: 02:43 pm
+Hora: 03:29 pm
 
 */
 
-// el signo ?, indica que parametro es opcional
-function nombreCompleto( nombre: string, apellido?: string) : string
-{
+// Indicar un valor de por defecto a un parametro, usamos el signo =, pero se convierte en opcional
+function nombreCompleto( nombre: string, apellido?: string, capitalizar: boolean = false) : string
+{    
+    console.log("capitalizar: "+capitalizar);
+
     // caso donde el apellido Si se recibe 
-    if( apellido )
+    if( capitalizar )
     {
-        return nombre+' '+apellido;
+        return capitalizarPalabras(nombre)+' '+capitalizarPalabras(apellido);
     }
 
     // caso donde el apellido No se recibe 
     else
     {
-        return nombre;
+        return nombre+ ' '+ apellido;
     }
+    
 }// end nombreCompleto
 
-let persona = nombreCompleto("Juan");
+function capitalizarPalabras( palabra: string ): string
+{
+    let palabraTransformada: string; 
+    palabraTransformada = palabra.charAt(0).toUpperCase() + palabra.substr(1).toLowerCase();
+    
+    return palabraTransformada;
+} // end capitalizarPalabras
 
-console.log(persona);
+// indicamos que el argumento tendrá un valor diferte al que se inidicó en la función
+let personaTransformada = nombreCompleto("juan", "dominguez", true);
+let personaOriginal = nombreCompleto("juan", "dominguez");
+
+console.log(personaTransformada);
+console.log(personaOriginal);
